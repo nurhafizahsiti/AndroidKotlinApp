7854b1630d8c4a5578ce7f6f46c61d8d37c631f4
nurhafizahsiti <nurhafizahsiti27@gmail.com>
2024-10-07 18:45:21 +0700

    add activity_main.xml

--- app/src/main/java/id/ac/polbeng/sitinurhafizah/androidkotlinapp/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/sitinurhafizah/androidkotlinapp/MainActivity.kt
@@ -3,47 +3,29 @@ package id.ac.polbeng.sitinurhafizah.androidkotlinapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import id.ac.polbeng.sitinurhafizah.androidkotlinapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvCounter: TextView
-    private lateinit var btnAddNumber: Button
-    private lateinit var btnSubtractNumber: Button
-    private lateinit var btnResetNumber: Button
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Inisialisasi TextView dan Button
-        tvCounter = findViewById(R.id.tvCounter)
-        btnAddNumber = findViewById(R.id.btnAddNumber)
-        btnSubtractNumber = findViewById(R.id.btnSubtractNumber)
-        btnResetNumber = findViewById(R.id.btnResetNumber)
-
-        // Set nilai awal tvCounter
-        tvCounter.text = "1"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCounter.text = "1"
     }
-
-    // Fungsi untuk menambah angka
     fun addNumber(v: View){
-        val currVal = tvCounter.text.toString().toInt()
+        val currVal = binding.tvCounter.text.toString().toInt()
         val nextVal = currVal + 1
-        tvCounter.text = nextVal.toString()
+        binding.tvCounter.text = nextVal.toString()
     }
 
-    // Fungsi untuk mengurangi angka
+    fun resetNumber(v: View){
+        binding.tvCounter.text = "0"
+    }
     fun subtractNumber(v: View){
-        val currVal = tvCounter.text.toString().toInt()
-        if (currVal > 0) {  // Agar nilai tidak menjadi negatif
-            val nextVal = currVal - 1
-            tvCounter.text = nextVal.toString()
-        }
-    }
-
-    // Fungsi untuk mereset angka ke nol
-    fun resetNumber(v: View) {
-        tvCounter.text = "0"
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        binding.tvCounter.text = nextVal.toString()
     }
 }
